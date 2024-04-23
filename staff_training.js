caefe19d7454c621fd9d3ee49530f7f678159fb5
chankalong <105831626+chankalong@users.noreply.github.com>
2024-04-23 17:12:50 +0800

    Add files via upload

---
+++ staff_training.js
@@ -0,0 +1,14 @@
+var uid =  drupalSettings.user.uid;
+if (uid != 0) {
+  var imhs_non_login_div = document.getElementById('imhs_non_login');
+  var imhs_member_content_parent_div = document.getElementById('imhs_member_content_parent');
+  var password = prompt("Please enter your password:");
+  if (password === "IMHStraining20240423") {
+    alert("Password correct!");
+  } else {
+    alert("Incorrect password. Redirecting to the homepage.");
+    window.location.href = "https://refresh.bokss.org.hk/"; // Replace with your homepage URL
+  }
+  imhs_non_login_div.style.display = 'none';
+  imhs_member_content_parent_div.style.display = '';
+}
