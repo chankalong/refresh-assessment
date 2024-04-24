7db568dde7f1614fb20df021694109a87f464280
chankalong <105831626+chankalong@users.noreply.github.com>
2024-04-24 09:30:08 +0800

    Update staff_training.js

--- staff_training.js
+++ staff_training.js
@@ -3,12 +3,14 @@ if (uid != 0) {
   var password = prompt("Please enter your password:");
   if (password === "IMHStraining20240423") {
     alert("Password correct!");
+    var imhs_non_login_div = document.getElementById('imhs_non_login');
+    var imhs_member_content_parent_div = document.getElementById('imhs_member_content_parent');
+    imhs_non_login_div.style.display = 'none';
+    imhs_member_content_parent_div.style.display = '';  
   } else {
     alert("Incorrect password. Redirecting to the homepage.");
     window.location.href = "https://refresh.bokss.org.hk/"; // Replace with your homepage URL
+    document.getElementById('imhs_member_content_parent').remove();
   }
-  var imhs_non_login_div = document.getElementById('imhs_non_login');
-  var imhs_member_content_parent_div = document.getElementById('imhs_member_content_parent');
-  imhs_non_login_div.style.display = 'none';
-  imhs_member_content_parent_div.style.display = '';  
+  
 }
