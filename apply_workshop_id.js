3eb4399237ae46f78e6256479fa0c613bdcc7d6c
chankalong <105831626+chankalong@users.noreply.github.com>
2024-05-03 12:34:29 +0800

    Add files via upload

---
+++ apply_workshop_id.js
@@ -0,0 +1,23 @@
+var workshop_link_textbox = document.getElementById("workshop_link");
+var member_id_textbox = document.getElementById("member_id");
+var form_apply_workshop_member_id = document.getElementById(
+  "form_apply_workshop_member_id"
+);
+
+workshop_link_textbox = document.referrer.replace(
+  "https://refresh.bokss.org.hk",
+  ""
+);
+member_id_textbox = drupalSettings.user.member_id;
+
+form_apply_workshop_member_id.addEventListener("submit", function (e) {
+  var data = new FormData(form_apply_workshop_member_id);
+  var action = e.target.action;
+  fetch(action, {
+    method: "POST",
+    body: data,
+  });
+  e.preventDefault();
+});
+
+document.querySelector("input[value=提交工作坊資料]").click();
