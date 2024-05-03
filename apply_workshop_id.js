aeb66c6173a7d18de01e209b4ed67d8e7ed4f65a
chankalong <105831626+chankalong@users.noreply.github.com>
2024-05-03 15:50:22 +0800

    Update apply_workshop_id.js

--- apply_workshop_id.js
+++ apply_workshop_id.js
@@ -3,12 +3,11 @@ var member_id_workshop_textbox = document.getElementById("member_id_workshop");
 var form_apply_workshop_member_id = document.getElementById(
   "form_apply_workshop_member_id"
 );
+
+workshop_link_textbox.value = document.referrer.replace("https://refresh.bokss.org.hk", "").replace("?utm_source=Refresh", "");
+member_id_workshop_textbox.value = drupalSettings.user.member_id;
+
 form_apply_workshop_member_id.addEventListener("submit", function (e) {
-  workshop_link_textbox.value = document.referrer.replace(
-  "https://refresh.bokss.org.hk",
-  "");
-  member_id_workshop_textbox.value = drupalSettings.user.member_id;
-  e.preventDefault();
   var data = new FormData(form_apply_workshop_member_id);
   var action = e.target.action;
   fetch(action, {
@@ -16,5 +15,5 @@ form_apply_workshop_member_id.addEventListener("submit", function (e) {
     body: data,
   });
   e.preventDefault();
-}, true);
+});
 document.querySelector("input[value=提交工作坊資料]").click();
