d40328551305562fd96bf944eb6725e4d71dea61
chankalong <105831626+chankalong@users.noreply.github.com>
2024-05-03 17:37:45 +0800

    Update apply_workshop_id.js

--- apply_workshop_id.js
+++ apply_workshop_id.js
@@ -3,8 +3,9 @@ var member_id_workshop_textbox = document.getElementById("member_id_workshop");
 var form_apply_workshop_member_id = document.getElementById(
   "form_apply_workshop_member_id"
 );
-
-workshop_link_textbox.value = document.referrer.replace("https://refresh.bokss.org.hk", "").replace("?utm_source=Refresh", "");
+url_path = document.referrer.replace("https://refresh.bokss.org.hk", "");
+url_para_position = url_path.search("?")
+workshop_link_textbox.value = url_path.slice(0, url_para_position);
 member_id_workshop_textbox.value = drupalSettings.user.member_id;
 
 form_apply_workshop_member_id.addEventListener("submit", function (e) {
