8e072e76d4be93e8e5385c6380ef3426d28d119d
chankalong <105831626+chankalong@users.noreply.github.com>
2024-06-17 09:56:14 +0800

    Add files via upload

---
+++ form-automate.js
@@ -0,0 +1,13 @@
+ 
+  var form = document.getElementById("form_big_five");
+
+  form.addEventListener("submit", function (e) {
+    var data = new FormData(form);
+    var action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    });
+    e.preventDefault();
+
+  });
