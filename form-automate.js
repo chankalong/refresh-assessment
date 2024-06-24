2e94f81a766186997eb494c0f143d89a21d503c7
chankalong <105831626+chankalong@users.noreply.github.com>
2024-06-24 11:30:41 +0800

    Update form-automate.js

--- form-automate.js
+++ form-automate.js
@@ -1,8 +1,9 @@
 var form = document.getElementById("form_big_five");
 form.onsubmit = function (e) {
-    e.preventDefault();
+    
     fetch(form.action, {
         method: "post", 
         body: new FormData(form)
     });
+    e.preventDefault();
 }
