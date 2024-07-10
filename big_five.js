1c9d12727af9d6f87a4c5dc308f972f38ec6810d
chankalong <105831626+chankalong@users.noreply.github.com>
2024-07-10 14:58:29 +0800

    Update big_five.js

--- big_five.js
+++ big_five.js
@@ -535,7 +535,7 @@ function clearTooltip(e) {
       margin: { b: 35, t: 45, r: 25, l: 25, pad: 0 },
       font: {
         family: "'Noto Sans HK', Arial, sans-serif",
-        size: 12
+        size: 14
       },
       polar: {
         bgcolor: "rgba(0,0,0,0)",
