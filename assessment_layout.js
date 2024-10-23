aab09bbf3bdf8f679dc37e9fc98ad277a58d95ec
chankalong <105831626+chankalong@users.noreply.github.com>
2024-10-23 17:34:27 +0800

    Update assessment_layout.js

--- assessment_layout.js
+++ assessment_layout.js
@@ -1,1 +1,12 @@
-var mixer = mixitup('#assessment_div');
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+  var mixer_count = 0;
+  if (mixer_count == 0) {
+    var mixer = mixitup("#assessment_div");
+    mixer_count = 1;
+  } else {
+    console.log("already mixer render");
+  }
+});
+
+
