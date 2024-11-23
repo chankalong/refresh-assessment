f8de7a7c1983b1f7866818338da75b132bb23e33
chankalong <105831626+chankalong@users.noreply.github.com>
2024-11-23 18:33:27 +0800

    Update phq_gad.js

--- phq_gad.js
+++ phq_gad.js
@@ -1,7 +1,12 @@
-let query_String = window.location.search;
+if (query_String) {
+  let query_String = window.location.search;
   var activity_name = new URLSearchParams(query_String).get('activity_name');
 var activity_name_textbox = document.getElementById("activity_name");
 activity_name_textbox.value = activity_name;  
+} else {
+  console.log('already query_String')
+}
+  
 
 function clearTooltip(e) {
     setTimeout(function () {
