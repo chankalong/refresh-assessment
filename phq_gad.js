2faadb1c0e97d30591807215f1eecfa3f0f70c92
chankalong <105831626+chankalong@users.noreply.github.com>
2024-11-23 18:35:23 +0800

    Update phq_gad.js

--- phq_gad.js
+++ phq_gad.js
@@ -1,5 +1,7 @@
+
+let query_String;
 if (query_String) {
-  let query_String = window.location.search;
+  query_String = window.location.search;
   var activity_name = new URLSearchParams(query_String).get('activity_name');
 var activity_name_textbox = document.getElementById("activity_name");
 activity_name_textbox.value = activity_name;  
