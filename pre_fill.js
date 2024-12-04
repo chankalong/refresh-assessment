56b355f099d65960c103f1aae20e34b4c5ac4366
chankalong <105831626+chankalong@users.noreply.github.com>
2024-12-04 10:57:51 +0800

    Update pre_fill.js

--- pre_fill.js
+++ pre_fill.js
@@ -1,10 +1,10 @@
+var counselling_apply_button = document.getElementById('counselling_apply_button')
+var original_link = counselling_apply_button.href
+
+if (window.location.pathname.includes('consultation')) {
   var queryString = window.location.search;
-  //var lms_course = new URLSearchParams(queryString).get('lms_course');
-  //var utm_campaign = new URLSearchParams(queryString).get('utm_campaign');
-  //if (lms_course == null) {
-  //  var lms_course_final = utm_campaign} else {
-  //    var lms_course_final = lms_course}
-  
-  var original_link = document.getElementsByClassName('btnRound btnRound-orange')[0].href
-  var replace_link = original_link.concat(queryString)
-  document.getElementsByClassName('btnRound btnRound-orange')[0].href = replace_link
+} else if (window.location.pathname.includes('workshop-contact-later')) {
+  var queryString = '?referral_utm=' + document.referrer
+} else {console.log('error')}
+var replace_link = original_link.concat(queryString)
+counselling_apply_button.href = replace_link
