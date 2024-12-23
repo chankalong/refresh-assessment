6ce3123c33ec8706fca31a012c55599da732937c
chankalong <105831626+chankalong@users.noreply.github.com>
2024-12-23 14:44:55 +0800

    Update phq.js

--- phq.js
+++ phq.js
@@ -348,7 +348,7 @@ form.addEventListener("submit", function (e) {
   var config = { responsive: true, displaylogo: false, displayModeBar: false };
 
   
-  Plotly.newPlot("phq_plotly_div", phq_data, layout, config);
+  Plotly.newPlot("myDiv", phq_data, layout, config);
   //document.getElementById('block-bokss-page-title').scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
 
   document
