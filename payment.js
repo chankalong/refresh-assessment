7c91081040763484f179857c16bc12c812ef089f
chankalong <105831626+chankalong@users.noreply.github.com>
2025-04-24 12:29:59 +0800

    Add files via upload

---
+++ payment.js
@@ -0,0 +1,15 @@
+const requestData = {
+        amount: 10,
+        description: "Hillman Group",
+        account_code: "account",
+        activity_code: "activity",
+      };
+      console.log('Initiating checkout with data:', requestData);
+
+      const response = await fetch('https://payment-gateway-five-rho.vercel.app/api/checkout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+      });
